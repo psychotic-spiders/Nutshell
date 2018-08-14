7318124edb79126d165fa7ba75a48729bcef70f6
Klaus Hardt <klausheina@gmail.com>
2018-08-14 11:56:24 -0500

    Klaus login form

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,23 @@ const logInPage = require("./LoginPage.js")
 const sessionActiveUser = require("./dataManager/sessionActiveUser")
 const articleManager = require("./dataManager/articlesManager")
 
-function articleList() { articleManager.getArticles().then(r =>{
-    r.forEach(entry => { console.log(entry)})
-})
+
+function articleList() {
+    articleManager.getArticles()
+    .then(r => {
+        r.forEach(entry => {
+            console.log(entry)})
+    })
 }
+
 articleList()
 logInPage()
+
+
+
+
+
+
+
+
+
