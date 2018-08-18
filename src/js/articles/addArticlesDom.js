2dd0d7cdb389914df80343242b3e6d60016a9b74
Jessteus <mateusvanhalen@gmail.com>
2018-08-18 14:09:06 -0500

    matt merge MVP

--- src/js/articles/addArticlesDom.js
+++ src/js/articles/addArticlesDom.js
@@ -5,38 +5,38 @@ const inputArticles = require("./inputArticles");
 const session = require("../dataManager/sessionActiveUser");
 const messageArticlesDOM = require("./messageArticlesDom")
 
-const saveArtcileToDatabase = function(){
+const saveArtcileToDatabase = function () {
 
-document.querySelector(".saveArticlebutton").addEventListener("click", () => {
+    document.querySelector(".saveArticlebutton").addEventListener("click", () => {
 
-    // get form field values, add timestamp, URL, Content
+        // get form field values, add timestamp, URL, Content
 
-const userObject = session.getActiveUser()
-// send data to session storage by category Matt
-    const newArticle = {
-        content: document.querySelector("#titleClass").value,
-        // userID: userObject.id,
-        // userName: userObject.username,
-        URL: document.querySelector("#URLClass").value,
-        date: Date.now(),
-        Synopsis: document.querySelector("#symClass").value,
-    }
+        const userObject = session.getActiveUser()
+        // send data to session storage by category Matt
+        const newArticle = {
+            content: document.querySelector("#titleClass").value,
+            // userID: userObject.id,
+            // userName: userObject.username,
+            URL: document.querySelector("#URLClass").value,
+            date: Date.now(),
+            Synopsis: document.querySelector("#symClass").value,
+        }
 
-    articleManager.saveArticleEntry(newArticle).then(() => {
-        inputArticles.clearArticleForm()
+        articleManager.saveArticleEntry(newArticle).then(() => {
+            inputArticles.clearArticleForm()
 
-        document.querySelector("#inputArticleDOM").innerHTML = "";
-        artcileManager.getArticles().then(result =>{
-            result.forEach(entry => {
+            document.querySelector("#inputArticleDOM").innerHTML = "";
+            artcileManager.getArticles().then(result =>{
+                result.forEach(entry => {
 
 
-                document.querySelector("#inputArticleDOM").innerHTML += messageArticlesDOM.articleHTML(entry)
+            document.querySelector("#inputArticleDOM").innerHTML += messageArticlesDOM.articleHTML(entry)
+        })
+
+            })
+        })
     })
-
-
-})
-})})}
-
+}
 
 module.exports = saveArtcileToDatabase
 
